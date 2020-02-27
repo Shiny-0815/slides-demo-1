d141982d481eb733a083962067db66e77c88bd8d
shiny <shiny.gao@foxmail.com>
2020-02-28 02:13:32 +0800

    优化后
    Signed-off-by: shiny <shiny.gao@foxmail.com>

--- main.js
+++ main.js
@@ -1,23 +1,41 @@
-$('.images >img:nth-child(1)').addClass('current')
-$('.images >img:nth-child(2)').addClass('enter')
-$('.images >img:nth-child(3)').addClass('enter')
+初始化()
 let n = 1
+let k=5  //轮播图片数量
 setInterval(() => {
-    $(`.images >img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
         .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+            makeEnter($(e.currentTarget))
         })
-    $(`.images >img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n+1))
     n += 1
 }, 3000);
 
+//函数
 function x(n) {
-    if (n > 3) {
-        n = n % 3
+    if (n > k) {
+        n = n % k
         if (n === 0) {
-            n = 3
+            n = k
         }
     }
     return n
 }
 
+//初始化
+function 初始化() {
+    $('.images >img:nth-child(1)').addClass('current').siblings().addClass('enter')
+}
+//获取图片
+function getImage(n) {
+    return $(`.images >img:nth-child(${x(n)})`)
+}
+//状态转换
+function makeCurrent($node) {
+    return $node.removeClass('enter').addClass('current')
+}
+function makeLeave($node) {
+    return $node.removeClass('current').addClass('leave')
+}
+function makeEnter($node) {
+    return $node.removeClass('leave').addClass('enter')
+}
